27888571176d6c1685c6e6987d712312569f2f22
Osmiogrzesznik <boleskok@gmail.com>
2017-05-19 06:56:42 +0100

    Create Ajax_abstrakt.js
    Skrypt zawiera dwie funkcje:
    1>-przygotuj do wysłania danych ()
     Funkcja akceptuję  obiekt  Jako parametru oraz jako parametr domyślny dodawany z przodu ciągu ,
    Zwraca: sformatowany ciąg kodowany w standardowym formacie URI
    2>- funkcja wyślij ()
    Akceptuje dwa parametry: 
    Url-  adres  docelowego skryptu PHP
    StringDanych-   Ciąg danych !!!musi być sformatowany w Url
    Funkcją tworzy  "zadanie"instancję klasy (?)xmlhttprequest , ktora kontaktuje się z podanym adresem za pomocą metody POST 
    Dodaje nasłuch na odpowiedź nie przeszkadzajac czemukolwiek co się dzieje na stronie w międzyczasie(asynchronicznie)

---
+++ Ajax_abstrakt.js
@@ -0,0 +1,45 @@
+
+var url = "zapisz.php";// twoj skrypt przyjmujacy dane juz bez losowania
+
+
+//function przygotujParametryDoWysłania
+
+PrzygotujDoWyslania = function(obj, prefix) {
+  var str = [], p;
+  for(p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+      str.push((v !== PrzygotujDoWyslania && typeof v === "object") ?
+        PrzygotujDoWyslania(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+  }
+  return str.join("&");
+}
+
+
+//function przygotujParametryDoWysłania
+
+
+
+
+
+
+
+stringdanych = PrzygotujDoWyslania(mojObjektZDanymi);
+
+
+function wyslij(url,stringDanych){
+var zadanie = new XMLHttpRequest();
+zadanie.open("POST", url, true);
+
+//Send the proper header information along with the request
+zadanie.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+zadanie.onreadystatechange = function() {//wzywa funkcje gdy stan zadania sie zmienia
+    if(zadanie.readyState == 4 && zadanie.status == 200) {
+        wyswietl(zadanie.responseText);
+    }
+}
+zadanie.send(stringDanych);
+}
