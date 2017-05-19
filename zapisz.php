4ca1a887a05113702f1013022ca4de709b65c322
Jornarileesh <t.ciarachowicz@gmail.com>
2017-05-19 19:05:05 +0200

    show mi POST from JS - encounter 1

--- zapisz.php
+++ zapisz.php
@@ -1,3 +1,16 @@
-﻿<?php 
+﻿<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pokaż dane z JS-a</title>
+	<link rel="stylesheet" href="styles/style.css" type="text/css" />
+	<script type="text/javascript" src="Ajax_abstrakt.js"></script>
+</head>
+
+<body>
+
+<?php 
 echo $_POST['stringDanych'];
 ?>
+
+</body>
+</html>
