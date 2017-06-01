45f69f957e77c1bc2e7897f6077f5e6d7b76cef2
Osmiogrzesznik <boleskok@gmail.com>
2017-06-01 13:16:29 +0100

    Update kostka.php

--- Website/newUI/kostka.php
+++ Website/newUI/kostka.php
@@ -226,6 +226,32 @@ else
 			
         </section>
         
+
+<section>
+<div id="sramka">
+
+
+
+            <div style="border: 1px solid red; padding: 10px;background-color:white;text-align:center;">
+                <span style="color:red;background-color:white;"><h1>NO JAVASCRIPT</h1>
+You need it to see our new nice interface:
+<img src="http://imageshack.com/a/img923/2711/JQsUC6.jpg" width=70%>
+<h6>Unfortunately and unbelievably (in or after 2017) your browser still does not support Javascript or it is not enabled. <br>
+                <br>Go then and walk a dog and dont forget to come back with newer device. <br><br>Alternatively, you can use our oldschool version </h6>
+                <a href="/" style="background-color:lightgray;color:blue;">here</a>
+
+        </noscript>
+
+
+
+
+
+
+</div>
+
+</section>
+
+
         <footer>
             <section id="copyright">
             	<h3 class="hidden">Copyright notice</h3>
